05ee0c64d59e499fcf13d470c25d80c8f9c3f66d
adammallalieu <adammallalieu@adams-mbp.dhcp.wustl.edu>
2023-02-23 14:08:58 -0600

    flag!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,53 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.3);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(0.5, 0.5, 0.25, 0.3);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5, 0.5, 0.1, 0.3);
+		
+		StdDraw.setPenColor(0,140,0);
+		StdDraw.filledCircle(0.5, 0.5, 0.025);
+		
+		double [] x = {0.475,0.5,0.525};
+		double [] y = {0.5, 0.6, 0.5};
+		StdDraw.filledPolygon(x, y);
+		
+		double [] x2 = {0.5,0.6,0.525};
+		double [] y2 = {0.5, 0.55, 0.475};
+		StdDraw.filledPolygon(x2, y2);
+		
+		double [] x3 = {0.5,0.4,0.475};
+		double [] y3 = {0.5, 0.55, 0.475};
+		StdDraw.filledPolygon(x3, y3);
+		
+		double [] x4 = {0.5,0.6,0.5};
+		double [] y4 = {0.500, 0.475, 0.45};
+		StdDraw.filledPolygon(x4, y4);
+		
+		double [] x5 = {0.5,0.4,0.5};
+		double [] y5 = {0.500, 0.475, 0.45};
+		StdDraw.filledPolygon(x5, y5);
+		
+		StdDraw.filledRectangle(0.5, 0.45, 0.01, 0.05);
+
+		
+
+
+
+
+
+
+		
+
+
+		
+
+
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,48 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double X = in.nextDouble();
+		double Y = in.nextDouble();
+		double width = in.nextDouble();
+		double height = in.nextDouble();
+		
+		Color newcolor = new Color(red, green, blue);
+		StdDraw.setPenColor(newcolor);
+
+
+
+		if (shape.equals("rectangle")) {
+		if (filled == true) {
+
+
+		StdDraw.filledRectangle(X, Y, width, height);
+		
+		}
+		
+		else {
+			StdDraw.rectangle(X, Y, width, height);
+		}
+		}
+		
+		if ( shape.equals("ellipse") ) {
+			if (filled == true) {
+
+				
+				StdDraw.filledEllipse(X, Y, width, height);
+				
+				}
+				
+				else {
+					StdDraw.ellipse(X, Y, width, height);
+				}
+		}
 		
 	}
 }
